53fac7378ba3aa034d2f015aee4cc5896a50d7a2
eXtr3me <dmfigueira@gmail.com>
2019-12-23 22:08:26 +0000

    Updates

---
+++ precache-manifest.a11c09050700f355303af28bf0553fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6596180d0a1eff3c21e0bfe636007ddb",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "12bd4d4b20216c0ad288",
+    "url": "/monsters/static/css/main.848f4d5c.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12bd4d4b20216c0ad288",
+    "url": "/monsters/static/js/main.fd947317.chunk.js"
+  },
+  {
+    "revision": "82963c5b64b97fed9ed5",
+    "url": "/monsters/static/js/runtime-main.5229bf72.js"
+  }
+]);
